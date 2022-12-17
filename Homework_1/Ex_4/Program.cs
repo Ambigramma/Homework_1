f994a8d200a759494a14427364b93d3ad8e59643
Andres Karapetyan <illum.andres@gmail.com>
2022-12-17 17:01:50 +0400

    Task4

---
+++ Homework_1/Ex_4/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача2. Напишите программу, которая на вход принимает три числа и выдает, 
+// максимальное из этих чисел.
+
+int a = int.Parse(Console.ReadLine());
+int b = int.Parse(Console.ReadLine());
+int c = int.Parse(Console.ReadLine());
+
+int max = 0;
+
+while (max != 0);
+{
+    if(a > b & a > c) max = a;
+    if(b > a & b > c) max = b;
+    if(c > a & c > b) max = c;
+
+}
+
+Console.WriteLine($"max = {max}");
